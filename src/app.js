1b26b04ada6fc6410885dd5ddb89cbee96486f71
Elijah Guerrero <elijahguerrero97@gmail.com>
2020-01-08 00:57:53 -0600

    'added'

---
+++ src/app.js
@@ -0,0 +1,39 @@
+//  --- requirements ---
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config');
+
+//  --- middleware ---
+const app = express()
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+
+
+//  --- endpoints ---
+app.get('/', (req, res) => {
+    res.send('Hello, world!')
+});
+
+app.use((error, req, res, next) => {
+    let response
+    if (NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+  
+
+//  --- export ---
+module.exports = app;
+
